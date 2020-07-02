f77dddecd45de05b46781f972c3dab8964bce431
Vievk Bhadra <vivek.bhadra@gmail.com>
2020-07-02 23:57:29 +0100

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,14 @@
+# Machine Learning for Trading | Georgia Institute of Technology | Udacity
+# https://www.udacity.com/course/machine-learning-for-trading--ud501
+# Sample Program 1
+import pandas as pd
+
+def test_run():
+    """Function called by Test Run."""
+    df = pd.read_csv("data/AAPL.csv")
+    print df.head()
+
+
+if __name__ == "__main__":
+    test_run()
 
