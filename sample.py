886e4afa041ea2e572b04dcb9bbbd8d42e788f5b
Vievk Bhadra <vivek.bhadra@gmail.com>
2020-07-03 08:03:00 +0100

    Update sample.py

--- sample.py
+++ sample.py
@@ -35,3 +35,18 @@ def test_run():
 
 if __name__ == "__main__":
     test_run()
+
+# Sample Program 3
+"""Plot High prices for IBM"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def test_run():
+    df = pd.read_csv("data/IBM.csv")
+    df['High'].plot()
+    plt.show()  # must be called to show plots
+
+
+if __name__ == "__main__":
+    test_run()
